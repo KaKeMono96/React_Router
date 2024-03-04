d2d97e8c9fbeb25c4ba16914e7d4540fbacc8d59
lonelonec <ayeayeaung96@gmail.com>
2024-03-04 13:42:41 +0630

    first com

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+function About() {
+    return (   
+      <div>
+          <h1>About</h1>
+          <p>lorem4gcagsflehsgoe bdsjgfiwgfiwg bdagifgwig
+            dgaiwfhiesgjo jhdihfwiewjhog jhdwhqfhe okokok
+          </p>
+      </div>   
+    );
+  }
+  
+  export default About;
+  
---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+
+function Contact() {
+    return (   
+      <div>
+          <h1>Contact</h1>
+          <p>lorem4gcagsflehsgoe bdsjgfiwgfiwg bdagifgwig
+            dgaiwfhiesgjo jhdihfwiewjhog jhdwhqfhe okokok
+          </p>
+      </div>   
+    );
+  }
+  
+  export default Contact;
+  
